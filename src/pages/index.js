93916d5ff48822a38e212dc6de07e4ca1eae7b26
Peter Durham <peterdurham@protonmail.com>
2020-06-13 15:28:01 -0700

    cleaned up sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,7 @@ const IndexPage = () => {
         <div className="sidebar">
           <h2 className="sidebar-header">Mailing List</h2>
           <div className="sidebar-emails">
-            <h2>Specialized Mailing list header here</h2>
+            <h2>Mailing list here</h2>
             <p>Subscribe to my list for lots of great reasons</p>
             <form>
               <input type="text" id="email" />
